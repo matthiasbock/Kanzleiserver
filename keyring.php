5f9f4c8a1d2e1bc06b7ad997fcd3d2fbb772a0b5
Matthias <mail@matthiasbock.net>
2012-09-24 11:36:50 +0200

    keyring

--- keyring.php
+++ keyring.php
@@ -1,7 +1,15 @@
 <?php
 
-# request updates from authority or web of trust
-# display all user keys + trust
+# updates from authority or web of trust (via shared storage)
+
+# display web of trust keys
+
+include('verified_friends.php');
+
+# display pki keys
+
+include('verified_authorities.php');
+
 # allow to import / export
 # allow to change trust level
 
